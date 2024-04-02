e11bf5a84dbdb18891ba6cc6bcc92bf54622483b
Omkar <omkarrajan495@gmail.com>
2024-04-02 14:00:21 +0530

    handling the error

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,22 @@ import React from "react";
 
 const Home = async ({ searchParams }) => {
   const genre = searchParams.genre || "fetchTrending";
-  let data;
-  try {
+  
     const res = await fetch(
       `https://api.themoviedb.org/3${
         genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/movie/week`
       }?api_key=${API_KEY}&language=en-US&page=1`
     );
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
+    // if (!res.ok) {
+    //   throw new Error('Failed to fetch data');
+    // }
 
     const responseBody = await res.text();
 
     // Parse the response body as JSON if it's not empty
-    data = responseBody ? JSON.parse(responseBody) : null;
-  } catch (error) {
-    console.log(error);
-  }
+    const data = responseBody ? JSON.parse(responseBody) : null;
+
 
   const results = data && data.results ? data.results : [];
 
